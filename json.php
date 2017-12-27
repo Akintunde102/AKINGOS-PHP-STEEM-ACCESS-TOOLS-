53df8d9a03672233d5c94c745a8b988693eb8276
Akintunde102 <jegedeakintunde@gmail.com>
2017-12-27 23:37:23 +0100

    Update json.php

--- json.php
+++ json.php
@@ -5,7 +5,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>jQuery JSONPanel Plugin Demo</title>
+    <title>Steem JSON VIEWER</title>
     <link rel="stylesheet" type="text/css" href="css/demo.css">
     <link href="css/jsonpanel.css" rel="stylesheet" type="text/css">
    <script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
@@ -33,7 +33,7 @@
     </script>
   </head>
   <body>
-    <h1>jQuery JSONPanel Plugin Demo</h1></div>
+    <h1>Steem JSON VIEWER</h1></div>
     <div class="jsonpanel"></div>
   </body>
 </html>
